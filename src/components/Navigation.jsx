04b1ea6a2b990f37a64be354eae938ba97cb36f5
Ijeoma Igbokwe <preciousigbokwe377@gmail.com>
2023-10-21 01:30:04 +0300

    first commit

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { FiSearch } from "react-icons/fi";
 
 const Navigation = () => {
--- src/components/reuseableComponents/blogs/blog1.jsx
+++ src/components/reuseableComponents/blogs/blog1.jsx
@@ -8,9 +8,37 @@ const BlogList1 = ({
   date,
   text1,
   text2,
+
+  isSmallCard  = true
 }) => {
   return (
-    <div>
+    
+    <>
+    
+    {isSmallCard === "small" ? (<div>
+      {/* container 2 */}
+      <div className="border bg-white w-[500px] p-[15px] rounded-[20px] mb-[13px]">
+        <div className="flex items-center gap-[15px]">
+          <img src="/home/blog-img3.png" alt="" />
+          <div>
+            <p className="text-[22px] font-semibold">{title}</p>
+            <p className="text-[#7887A8] text-[16px] text-ellipsis">{text1}</p>
+          </div>
+        </div>
+
+        <div className="flex justify-between items-center mt-[20px]">
+          <p className=" flex gap-[10px]">
+            <span className="text-[#FF7D51] bg-[#facbbb9e] rounded-[8px] px-[15px] py-[8px]">
+              Maths
+            </span>
+            <span className="px-[15px] py-[8px] text-[#7551E9] bg-[#e1daf9] rounded-[8px]">
+              E-learning
+            </span>
+          </p>
+          <p className="text-[#0AAFFF]">Read Blog</p>
+        </div>
+      </div>
+    </div>) : (<div>
       {/* container 1 */}
 
       <div className="bg-[white] p-[15px] border w-[400px] rounded-[20px] ">
@@ -45,7 +73,8 @@ const BlogList1 = ({
       </div>
 
     
-    </div>
+    </div>) }
+    </>
   );
 };
 
