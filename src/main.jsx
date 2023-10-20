f3537370ec2be8b7d554481aab9bbda8aab2bffc
Juliet <okwygod201@gmail.com>
2023-10-20 22:47:51 +0100

    changed contex.js to context.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./Redux/store.js";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ContextProvider } from "./Contex/Context.js";
+import { ContextProvider } from "./Contex/Context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
