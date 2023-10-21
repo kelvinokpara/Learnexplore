550a5701f8aaff25e510e05cd08c2902fa86451b
Kelvin Okpara <77330261+kelvinokpara@users.noreply.github.com>
2023-10-21 05:32:24 +0100

    Update HomePage.jsx

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,7 +2,6 @@ import Button from "../../components/reuseableComponents/button/button";
 
 const HomePage = () => {
   return <div>Home
-    <Button bg="bg-white" texts="sign-up" text="text-black text-xs" border="border-[1px]" />
     
   </div>;
 };
