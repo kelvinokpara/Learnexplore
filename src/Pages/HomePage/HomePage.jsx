98fceed032c0d81c1284bf6d58ca38d1745a99a1
Kelvin Okpara <77330261+kelvinokpara@users.noreply.github.com>
2023-10-21 05:39:38 +0100

    Update HomePage.jsx

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -3,17 +3,6 @@ import reactIcon from "../../assets/react.svg"
 
 const HomePage = () => {
   return <div>Home
-    <BlogList1 
-    title={"rererer"}
-    image={reactIcon}
-    profile_image={reactIcon}
-    profile_name={"ereretet"}
-    date={"gefr"}
-    text1={"fhfgsefshefshdfghsfy"}
-    text2={"ygfjysgfjsgfygfdjsgjftsegfjegdfs"}
-  
-    isSmallCard={true}
-    />
   </div>;
 };
 
