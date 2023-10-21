f096ffdc9d27e929703cb35286835f6dba2996d4
Quadri <kazeemquadri75@gmail.com>
2023-10-21 07:13:39 +0100

    m

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -3,7 +3,6 @@ import Navigation from "../../components/Navigation"
 
 const HomePage = () => {
   return <div>Home
-    {/* <Button bg="bg-white" texts="sign-up" text="text-black text-xs" border="border-[1px]" /> */}
     <Navigation/>
   </div>;
 };
