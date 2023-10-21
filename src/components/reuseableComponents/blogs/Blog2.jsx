3f6cc96d35ae577a69a536b623345352710aca5c
Ijeoma Igbokwe <preciousigbokwe377@gmail.com>
2023-10-21 21:28:43 +0300

    first commit

--- src/components/reuseableComponents/blogs/Blog2.jsx
+++ src/components/reuseableComponents/blogs/Blog2.jsx
@@ -1,20 +1,22 @@
 import BlogList1 from "./components/reuseableComponents/blogs/blog1";
-import reactIcon from "../../../assets"
+import reactIcon from "../../../assets";
 
 const HomePage = () => {
-  return <div>Home
-    <BlogList1 
-    title={"rererer"}
-    image={reactIcon}
-    profile_image={reactIcon}
-    profile_name={"ereretet"}
-    date={"gefr"}
-    text1={"fhfgsefshefshdfghsfy"}
-    text2={"ygfjysgfjsgfygfdjsgjftsegfjegdfs"}
-  
-    isSmallCard={true}
-    />
-  </div>;
+  return (
+    <div>
+      Home
+      <BlogList1
+        title={"rererer"}
+        image={reactIcon}
+        profile_image={reactIcon}
+        profile_name={"ereretet"}
+        date={"gefr"}
+        text1={"fhfgsefshefshdfghsfy"}
+        text2={"ygfjysgfjsgfygfdjsgjftsegfjegdfs"}
+        isSmallCard={true}
+      />
+    </div>
+  );
 };
 
 export default HomePage;
