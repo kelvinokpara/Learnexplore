885af29f331da217dacc81d962b3357844a2103c
Ijeoma Igbokwe <preciousigbokwe377@gmail.com>
2023-10-27 14:58:57 +0300

    gfd

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,7 +1,9 @@
 import Hero from "../../components/reuseableComponents/Hero/Hero";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <div>
+    
+  </div>;
 };
 
 export default HomePage;
