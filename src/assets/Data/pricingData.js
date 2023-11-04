52590d96ff30274ab6a25aae369c02f4aa7c52b9
unknown <okparakelvin65@gmail.com>
2023-11-04 22:54:52 +0100

    resolved import issues

--- src/assets/Data/pricingData.js
+++ src/assets/Data/pricingData.js
@@ -1,40 +1,46 @@
 import { amazonpay, UPI, mastercard, paytm } from "../Icons/payment-icons";
-
+import {
+  pricingCheck,
+  pricingInfo,
+  pricingMonthly,
+  pricingYearly,
+  pricingSingle,
+} from "../Icons/pricing-icons/index.js";
 export const pricingData = [
   {
-    mainIcon: "../Icons/pricing-icons/pricing-single.png",
+    mainIcon: { pricingSingle },
     priceType: "Single Course",
     price: "10$",
     btnText: "Try for free",
-    infoIcon: "../Icons/pricing-icons/pricing-info.png",
+    infoIcon: { pricingInfo },
     info: "Pay monthly, cancel anytime.",
-    featureIcon: "../Icons/pricing-icons/pricing-check.png",
+    featureIcon: { pricingCheck },
     features1: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
     features2: "sit amet, consectetur adipiscing.",
     features3:
       "Lorem ipsum dolor sit amet,adipiscing elit. Nullam accumsan ut arcu.",
   },
   {
-    mainIcon: "../Icons/pricing-icons/pricing-monthly.png",
+    mainIcon: { pricingMonthly },
     priceType: "Monthly Plan",
     price: "55$",
     btnText: "Try for free",
-    infoIcon: "../Icons/pricing-icons/pricing-info.png",
+    infoIcon: { pricingInfo },
     info: "Pay monthly, cancel anytime.",
-    featureIcon: "../Icons/pricing-icons/pricing-check.png",
+    featureIcon: { pricingCheck },
     features1: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
     features2: "sit amet, consectetur adipiscing.",
     features3:
       "Lorem ipsum dolor sit amet,adipiscing elit. Nullam accumsan ut arcu.",
   },
   {
-    mainIcon: "../Icons/pricing-icons/pricing-yearly.png",
-    priceType: "Single Course",
+    mainIcon: { pricingYearly },
+    priceType: "Yearly Plan",
     price: "40$",
     btnText: "Try for free",
-    infoIcon: "../Icons/pricing-icons/pricing-info.png",
+    infoIcon: { pricingInfo },
     info: "Pay monthly, cancel anytime.",
-    featureIcon: "../Icons/pricing-icons/pricing-check.png",
+    featureIcon: { pricingCheck },
     features1: "Lorem ipsum dolor sit amet, consectetur adipiscing.",
     features2: "sit amet, consectetur adipiscing.",
     features3:
