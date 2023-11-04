53d11a9565edf4b619ee37dc00e33ab07de09e36
unknown <okparakelvin65@gmail.com>
2023-11-04 05:40:04 +0100

    input test success

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,11 +1,28 @@
 import Input from "../../Components/reuseableComponents/inputs/Input";
-import CiMail from "react-icons/ci";
+import { CiMail } from "react-icons/ci";
 
 const HomePage = () => {
   return (
     <div>
       <div className="w-2/5">
-        <Input type={"text"} useType="auth" label={"email"} />
+        <Input
+          type={"email"}
+          useType="auth"
+          label={"email address"}
+          icon={<CiMail />}
+        />
+        <Input
+          type={"text"}
+          useType="contact"
+          label={"Contact address"}
+          icon={<CiMail />}
+        />
+        <Input
+          type={"text"}
+          useType="textarea"
+          label={"Message"}
+          icon={<CiMail />}
+        />
         <CiMail />
       </div>
     </div>
