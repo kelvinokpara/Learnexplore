97b1989d3180dd0bc0373792a19faf98fa6e8e29
unknown <okparakelvin65@gmail.com>
2023-11-07 13:45:03 +0100

    made changes to the reset password button

--- src/components/ResetPassword/ResetPassword.jsx
+++ src/components/ResetPassword/ResetPassword.jsx
@@ -7,33 +7,34 @@ import { useState } from "react";
 import CreatePassword from "../CreatePassword/CreatePassword";
 const ResetPassword = () => {
   const [email, setEmail] = useState("");
-  const [resetPassword, setResetPassword] = useState(false)
-  const [error, setError] =useState("")
+  const [resetPassword, setResetPassword] = useState(false);
+  const [error, setError] = useState("");
 
   const emailHandler = (e) => {
     const { name, value } = e.target;
 
     if (name === "email") {
       setEmail(value);
+      console.log(email, "ll");
     }
   };
 
-  const resetHandler =()=>{
-
-    if(email === ""){
-        setError("input field cannot be empty")
-        return(setTimeout(()=>{
-            setError("")
-        },3000))
-    }else{
-        setError("")
+  const resetHandler = () => {
+    console.log("yaya toure");
+    if (email === "") {
+      alert("hola");
+      setError("input field cannot be empty");
+      return setTimeout(() => {
+        setError("");
+      }, 3000);
+    } else {
+      setError("");
     }
-    
-     console.log(email)
 
-    
-     setResetPassword(true)
-  }
+    console.log(email);
+
+    setResetPassword(true);
+  };
   return (
     <div className="p-0 m-10 border-box">
       <div className="flex justify-center  flex-wrap items-center w-10/12 mx-auto my-0 h-[500px]">
@@ -43,15 +44,18 @@ const ResetPassword = () => {
           </div>
         </div>
         {!resetPassword && (
-            <div className="w-full md:w-[50%] lg:w-[50%] h-full ">
+          <div className="w-full md:w-[50%] lg:w-[50%] h-full ">
             <div className=" w-8/12 md:w-6/12 lg:w-6/12 mx-auto mt-20 h-[300px]">
               <div className="flex justify-start mb-2 items-center gap-2">
                 <FaAngleLeft />
                 <p className="font-poppins">Back</p>
               </div>
-              <p className="font-bold font-inter pb-2 text-2xl">Reset Password</p>
+              <p className="font-bold font-inter pb-2 text-2xl">
+                Reset Password
+              </p>
               <p className="text-xs pb-4 font-poppins">
-                please input your registered email to receive password reset token
+                please input your registered email to receive password reset
+                token
               </p>
               <div className="">
                 <Input
@@ -64,18 +68,19 @@ const ResetPassword = () => {
                   placeHolder={"Email"}
                 />
               </div>
-              <p className="text-xs pb-4 text-red-400 font-poppins italic">{error}</p>
-              <Button defButton="blue" onClick={resetHandler} width="full">
-                Reset Password
-              </Button>
+              <p className="text-xs pb-4 text-red-400 font-poppins italic">
+                {error}
+              </p>
+              <div onClick={resetHandler}>
+                <Button defButton="blue" width="full">
+                  Reset Password
+                </Button>
+              </div>
             </div>
           </div>
         )}
 
-        {resetPassword && (
-            <CreatePassword/>
-        )}
-        
+        {resetPassword && <CreatePassword />}
       </div>
     </div>
   );
