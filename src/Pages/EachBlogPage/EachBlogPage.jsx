98b529be2d636e258daa5b53f6b689fe62acf31c
Ijeoma Igbokwe <preciousigbokwe377@gmail.com>
2023-11-10 17:16:40 +0200

    blogPage

---
+++ src/Pages/EachBlogPage/EachBlogPage.jsx
@@ -0,0 +1,11 @@
+import EachBlog from "../../components/reuseableComponents/blog-page/EachBlog"
+
+const EachBlogPage = () => {
+  return (
+    <div>
+        <EachBlog/>
+    </div>
+  )
+}
+
+export default EachBlogPage
